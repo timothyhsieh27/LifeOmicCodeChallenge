69fad136727150e9d197be954efdcca4437793a8
Tim <timothyhsieh27@gmail.com>
2019-01-27 07:20:48 -0500

    Added server configuration.

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  /* created model loading here */
+  Task = require('./api/models/todoListModel'),
+  bodyParser = require('body-parser');
+
+/* mongoose instance connection url connection */
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/* Import and register route */
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
 
 app.listen(port);
 
 console.log('RESTful API server started on: ' + port);
+
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
